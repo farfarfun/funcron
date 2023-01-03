705a5fe2f93366810c979d8003ae4624222b696a
niuliangtao <niuliangtao@qq.com>
2023-01-03 15:52:38 +0800

    add

--- notecron/airflow/dags/notecoin/notecoin_binance_load_daily.py
+++ notecron/airflow/dags/notecoin/notecoin_binance_load_daily.py
@@ -1,59 +1,45 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from ccxt import binance
-from notecoin.coins.base.file import DataFileProperty
-
-path_root = '/home/bingtao/workspace/tmp'
+from notecoin.task.load import (LoadKlineDailyTask, LoadKlineWeeklyTask,
+                                LoadTradeDailyTask)
 
 
-def parse_day(ds=None):
-    ds = ds or '2022-09-01'
-    first = datetime.strptime(ds, '%Y-%m-%d') - timedelta(days=1)
-    last = first + timedelta(days=1) - timedelta(seconds=1)
-    return first, last
+def load_kline_daily(ds, *args, **kwargs):
+    LoadKlineDailyTask().refresh(ds)
 
 
-def parse_week(ds=None):
-    ds = ds or '2022-12-14'
-    first = datetime.strptime(ds, '%Y-%m-%d')
-    first = first - timedelta(days=first.weekday()) - timedelta(days=1)
-    first = datetime(first.year, first.month, first.day)
-    last = first + timedelta(weeks=1) - timedelta(seconds=1)
-    return first, last
-
-
-def load_kline(ds, *args, **kwargs):
-    start, end = parse_day(ds)
-    file_pro = DataFileProperty(exchange=binance(), path=path_root)
-    file_pro.file_format = '%Y%m%d'
-    file_pro.change_data_type('kline')
-    file_pro.change_timeframe('1m')
-    file_pro.change_freq('daily')
-    file_pro.load_daily(start, end)
+def load_kline_weekly(ds, *args, **kwargs):
+    LoadKlineWeeklyTask().refresh(ds)
 
 
 def load_trade(ds, *args, **kwargs):
-    start, end = parse_day(ds)
-    file_pro = DataFileProperty(exchange=binance(), path=path_root)
-    file_pro.file_format = '%Y%m%d'
-    file_pro.change_data_type('trade')
-    file_pro.change_timeframe('detail')
-    file_pro.change_freq('daily')
-    file_pro.load_daily(start, end)
+    LoadTradeDailyTask().refresh(ds)
 
 
-default_args = {"owner": "bingtao", "start_date": datetime(2022, 12, 1)}
+with DAG("notecoin-binance-load-daily",
+         description="notecoin",
+         default_args={"owner": "bingtao", "start_date": datetime(2021, 9, 1)},
+         schedule_interval='0 8 * * *') as dag:
+    t1 = PythonOperator(dag=dag,
+                        task_id='notecoin-binance-load-daily-kline',
+                        # provide_context=False,
+                        python_callable=load_kline_daily,
+                        op_args=[],
+                        op_kwargs={'keyword_argument': 'which will be passed to function'},
+                        depends_on_past=True,
+                        wait_for_downstream=True)
 
-dag = DAG("notecoin-binance-load-daily", description="notecoin",
-          default_args=default_args, schedule_interval='0 8 * * *')
-
-t1 = PythonOperator(dag=dag,
-                    task_id='notecoin-binance-load-daily-kline',
-                    #provide_context=False,
-                    python_callable=load_kline,
-                    op_args=[],
-                    op_kwargs={'keyword_argument': 'which will be passed to function'},
-                    depends_on_past=True,
-                    wait_for_downstream=True)
+with DAG("notecoin-binance-load-weekly",
+         description="notecoin",
+         default_args={"owner": "bingtao", "start_date": datetime(2021, 9, 1)},
+         schedule_interval='0 10 * * 2') as dag:
+    t2 = PythonOperator(dag=dag,
+                        task_id='notecoin-binance-load-weekly-kline',
+                        # provide_context=False,
+                        python_callable=load_kline_weekly,
+                        op_args=[],
+                        op_kwargs={'keyword_argument': 'which will be passed to function'},
+                        depends_on_past=True,
+                        wait_for_downstream=True)
